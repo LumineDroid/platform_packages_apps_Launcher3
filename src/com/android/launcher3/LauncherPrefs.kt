41222ebde327589c9e37e8a32bd445949987815e
StarkDroid <gamerprince.exp@gmail.com>
2022-11-29 15:48:29 +0800

    Launcher3: Add toggle to show/hide app drawer search bar
    Change-Id: I66bb9a232e7e49a94a1c8b11bea195a5d8faa6d1
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -264,6 +264,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val WORKSPACE_LOCK = backedUpItem("pref_workspace_lock", false)
         @JvmField val ENABLE_MINUS_ONE = backedUpItem("pref_enable_minus_one", true)
         @JvmField val ALLOW_WALLPAPER_ZOOMING = backedUpItem("pref_allow_wallpaper_zooming", true)
+        @JvmField val DRAWER_SEARCH = backedUpItem("pref_drawer_search", true)
         @JvmField val FONT_SIZE = backedUpItem("pref_custom_font_size", 100)
         @JvmField val ICON_SIZE = backedUpItem("pref_custom_icon_size", 100)
         @JvmField val RECENTS_MEMINFO = backedUpItem("pref_recents_meminfo", false)
--- src/com/android/launcher3/settings/SettingsAppDrawer.java
+++ src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -16,8 +16,11 @@
 
 package com.android.launcher3.settings;
 
+import android.content.SharedPreferences;
+
 import androidx.preference.Preference;
 
+import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.graphics.ThemeManager;
@@ -32,7 +35,8 @@ public class SettingsAppDrawer extends SettingsCategoryActivity {
         return getString(R.string.app_drawer_settings_fragment_name);
     }
 
-    public static class AppDrawerSettingsFragment extends CategorySettingsFragment {
+    public static class AppDrawerSettingsFragment extends CategorySettingsFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
 
         private Preference mThemeAllAppsIconsPref;
 
@@ -53,8 +57,24 @@ public class SettingsAppDrawer extends SettingsCategoryActivity {
         @Override
         public void onResume() {
             super.onResume();
+            getPreferenceManager().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
             if (mThemeAllAppsIconsPref != null) {
                 updateThemeAllAppsIconsPref();
+            }
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceManager().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (LauncherPrefs.DRAWER_SEARCH.getSharedPrefKey().equals(key)) {
+                LauncherAppState.INSTANCE.get(getContext()).setNeedsRestart();
             }
         }
 
