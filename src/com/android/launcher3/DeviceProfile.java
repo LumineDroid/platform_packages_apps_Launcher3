c5c9e2f5e699cc6da5c93d21152a522402566999
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-27 07:41:48 +0530

    Launcher3: Improve meminfo view height and margins
    - Take account of meminfo view height in overview to avoid overlapping
      in landscape mode.
    - Borrow the overview actions' bottom margin instead of hardcoding it.
    
    Change-Id: I4e5efb63ebfa0cb35bcaf2fb1008a6655e567de6
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -122,10 +122,6 @@ public class DeviceProfile {
     private final BottomSheetProfile mBottomSheetProfile;
     private FolderProfile mFolderProfile;
 
-    // Meminfo in overview
-    public int memInfoMarginGesturePx;
-    public int memInfoMarginTransientTaskbarPx;
-    public int memInfoMarginThreeButtonPx;
 
     private AllAppsProfile mAllAppsProfile;
     private final OverviewProfile overviewProfile;
@@ -144,6 +140,10 @@ public class DeviceProfile {
 
     // Taskbar
     private TaskbarProfile mTaskbarProfile;
+
+    // Meminfo in overview
+    public int memInfoHeight;
+
 
     /** Used only as an alternative to mocking when null values cannot be used. */
     @VisibleForTesting
@@ -198,8 +198,7 @@ public class DeviceProfile {
         mTypeIndex = 0;
         mIsResponsiveGrid = false;
         mDropTargetProfile = new DropTargetProfile(0, 0, 0, 0, 0, 0, 0, 0, 0);
-        memInfoMarginGesturePx = 0;
-        memInfoMarginThreeButtonPx = 0;
+        memInfoHeight = 0;
         mViewScaleProvider = null;
         mAllAppsProfile = new AllAppsProfile(new Point(0, 0), 0, 0, 0f, 0, 0, 0, 0, 0, 0,
                 new Rect(), 0, 0);
@@ -306,12 +305,6 @@ public class DeviceProfile {
                         qsbHeight
                 );
 
-        memInfoMarginGesturePx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_gesture);
-        memInfoMarginTransientTaskbarPx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_transient_taskbar);
-        memInfoMarginThreeButtonPx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_three_button);
 
         int allAppsTopPadding = mDeviceProperties.getInsets().top;
 
@@ -379,6 +372,9 @@ public class DeviceProfile {
                     mResponsiveAllAppsHeightSpec.getAvailableSpace(),
                     mResponsiveWorkspaceCellSpec);
         }
+
+        memInfoHeight = LauncherPrefs.RECENTS_MEMINFO.get(context) ? res.getDimensionPixelSize(
+                R.dimen.meminfo_claimed_height) : 0;
 
         mWorkspaceProfile = WorkspaceProfile.Factory.createWorkspaceProfile(
                 /*context*/ context,
@@ -1129,7 +1125,7 @@ public class DeviceProfile {
         int overviewActionsSpace = mDeviceProperties.isLargeScreen()
                 ? 0
                 : (overviewProfile.getActionsTopMarginPx() + overviewProfile.getActionsHeight());
-        return overviewActionsSpace + getOverviewActionsClaimedSpaceBelow();
+        return overviewActionsSpace + memInfoHeight + getOverviewActionsClaimedSpaceBelow();
     }
 
     /**
--- src/com/android/launcher3/settings/SettingsRecents.java
+++ src/com/android/launcher3/settings/SettingsRecents.java
@@ -16,6 +16,10 @@
 
 package com.android.launcher3.settings;
 
+import android.content.SharedPreferences;
+
+import com.android.launcher3.LauncherAppState;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 
 /**
@@ -28,11 +32,33 @@ public class SettingsRecents extends SettingsCategoryActivity {
         return getString(R.string.recents_settings_fragment_name);
     }
 
-    public static class RecentsSettingsFragment extends CategorySettingsFragment {
+    public static class RecentsSettingsFragment extends CategorySettingsFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
 
         @Override
         protected int getPreferencesXmlResId() {
             return R.xml.launcher_recents_preferences;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceManager().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceManager().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (LauncherPrefs.RECENTS_MEMINFO.getSharedPrefKey().equals(key)) {
+                LauncherAppState.INSTANCE.get(getContext()).setNeedsRestart();
+            }
         }
     }
 }
