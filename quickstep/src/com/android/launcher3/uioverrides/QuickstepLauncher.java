b8c80af4b4d84e323705807843559b3119a08b93
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-04 08:52:33 +0800

    Launcher3: Integrate LMOFreeform to system shortcut
    @ neobuddy89: Rename this to FREE_FORM to FLOATING following TaskShortcutFactory.
    @ adithya2306: De-duplicate code, fix launching profile apps, skip non-resizeable apps.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I8a5629e82f10e2c4ef08e23465e97fd010769e72
    (cherry picked from commit 66123cee79650cda08b13a906accc95c7641c73e)

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -59,6 +59,7 @@ import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.APP_LOCK;
 import static com.android.launcher3.popup.SystemShortcut.BUBBLE_SHORTCUT;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
+import static com.android.launcher3.popup.SystemShortcut.FLOATING;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.REMOVE;
@@ -574,6 +575,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         }
 
         shortcuts.addAll(getSplitShortcuts());
+        shortcuts.add(FLOATING);
         shortcuts.add(WellbeingModel.PAUSE_APPS);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -32,7 +32,10 @@ import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.Person;
 import android.app.WallpaperManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -50,6 +53,7 @@ import android.os.DeadObjectException;
 import android.os.Handler;
 import android.os.Message;
 import android.os.TransactionTooLargeException;
+import android.os.UserHandle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -973,5 +977,39 @@ public final class Utilities {
 
     public static int getAllAppsAlpha(Context context) {
         return LauncherPrefs.APP_DRAWER_OPACITY.get(context) * 255 / 100;
+    }
+
+    private static final String FREEFORM_PACKAGE = "com.libremobileos.freeform";
+    private static final String FREEFORM_INTENT = "com.libremobileos.freeform.START_FREEFORM";
+
+    public static boolean isResizeableActivity(Context context, ComponentName activity) {
+        if (activity == null) return false;
+        final ActivityInfo info;
+        try {
+            info = context.getPackageManager().getActivityInfo(activity, /* flags */ 0);
+        } catch (Exception e) {
+            Log.e(TAG, "failed to check isResizeableActivity:", e);
+            return false;
+        }
+        return ActivityInfo.isResizeableMode(info.resizeMode);
+    }
+
+    public static void startLmoFreeform(Context context, ComponentName activity,
+            int userId, int taskId) {
+        final Intent intent = new Intent(FREEFORM_INTENT)
+                .setPackage(FREEFORM_PACKAGE)
+                .putExtra("packageName", activity.getPackageName())
+                .putExtra("activityName", activity.getClassName())
+                .putExtra("userId", userId)
+                .putExtra("taskId", taskId);
+        context.sendBroadcast(intent);
+    }
+
+    public static void startLmoFreeform(Context context, ComponentName activity, int userId) {
+        startLmoFreeform(context, activity, userId, -1);
+    }
+
+    public static void startLmoFreeform(Context context, ComponentName activity) {
+        startLmoFreeform(context, activity, UserHandle.myUserId());
     }
 }
