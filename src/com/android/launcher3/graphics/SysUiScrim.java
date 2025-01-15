7e7e53f9de54b16053ccce53f13bb3e027c0365e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-01-15 18:13:50 +0100

    fixup! Launcher3: Allow hiding top shadow on statusbar
    Recreate scrim masks on setting change. Otherwise we risk hitting
    an NPE if the bitmaps are initialized with the setting disabled.
    
    Change-Id: Ife042cf5fdaeeb7f22a10a5514ee6dae6a89cac0
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/launcher3/graphics/SysUiScrim.java
+++ src/com/android/launcher3/graphics/SysUiScrim.java
@@ -81,12 +81,12 @@ public class SysUiScrim implements View.OnAttachStateChangeListener,
 
     private final RectF mTopMaskRect = new RectF();
     private final Paint mTopMaskPaint = new Paint(FILTER_BITMAP_FLAG | DITHER_FLAG);
-    private final Bitmap mTopMaskBitmap;
+    private Bitmap mTopMaskBitmap;
     private final int mTopMaskHeight;
 
     private final RectF mBottomMaskRect = new RectF();
     private final Paint mBottomMaskPaint = new Paint(FILTER_BITMAP_FLAG | DITHER_FLAG);
-    private final Bitmap mBottomMaskBitmap;
+    private Bitmap mBottomMaskBitmap;
     private final int mBottomMaskHeight;
 
     private final View mRoot;
@@ -108,14 +108,7 @@ public class SysUiScrim implements View.OnAttachStateChangeListener,
         SharedPreferences prefs = LauncherPrefs.getPrefs(view.getContext());
         mHideSysUiScrim = !prefs.getBoolean(KEY_SHOW_TOP_SHADOW, true)
                 || Themes.getAttrBoolean(view.getContext(), R.attr.isWorkspaceDarkText);
-
-        mTopMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mTopMaskHeight,
-                new int[]{0x3DFFFFFF, 0x0AFFFFFF, 0x00FFFFFF},
-                new float[]{0f, 0.7f, 1f});
-        mTopMaskPaint.setColor(0xFF222222);
-        mBottomMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mBottomMaskHeight,
-                new int[]{0x00FFFFFF, 0x2FFFFFFF},
-                new float[]{0f, 1f});
+        createMaskBitmaps();
 
         if (!mHideSysUiScrim) {
             view.addOnAttachStateChangeListener(this);
@@ -195,8 +188,19 @@ public class SysUiScrim implements View.OnAttachStateChangeListener,
         if (key.equals(KEY_SHOW_TOP_SHADOW)) {
             mHideSysUiScrim = !prefs.getBoolean(KEY_SHOW_TOP_SHADOW, true)
                     || Themes.getAttrBoolean(mRoot.getContext(), R.attr.isWorkspaceDarkText);
+            createMaskBitmaps();
             mRoot.invalidate();
         }
+    }
+
+    private void createMaskBitmaps() {
+        mTopMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mTopMaskHeight,
+                new int[]{0x3DFFFFFF, 0x0AFFFFFF, 0x00FFFFFF},
+                new float[]{0f, 0.7f, 1f});
+        mTopMaskPaint.setColor(0xFF222222);
+        mBottomMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mBottomMaskHeight,
+                new int[]{0x00FFFFFF, 0x2FFFFFFF},
+                new float[]{0f, 1f});
     }
 
     /**
