Evolution-X-AOSP/packages_apps_Launcher3

Launcher3: Add toggle to show/hide app drawer search bar
Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
Signed-off-by: minaripenguin <alexfinhart@gmail.com>
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -168,6 +168,12 @@ public class ActivityAllAppsContainerView<T extends Context & AppLauncher
           getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
         }
         
+        if (Utilities.showSearchBar(getContext())) {
+            mSearchContainer.setVisibility(View.VISIBLE);
+        } else {
+            mSearchContainer.setVisibility(View.GONE);
+        }
+
         mSearchUiManager = (SearchUiManager) mSearchContainer;
         mSearchUiManager.initializeSearch(this);
     }

LumineDroid/platform_packages_apps_Launcher3

Change-Id: I0c03fde326700fe826f249f7c813034d27900b9f
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -302,6 +302,12 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     protected void onFinishInflate() {
         super.onFinishInflate();
 
+        if (Utilities.showSearch(getContext())) {
+            mSearchContainer.setVisibility(View.VISIBLE);
+        } else {
+            mSearchContainer.setVisibility(View.GONE);
+        }
+
         mAH.get(SEARCH).setup(mSearchRecyclerView,
                 /* Filter out A-Z apps */ itemInfo -> false);
         rebindAdapters(true /* force */);